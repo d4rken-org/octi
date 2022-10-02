43d977c5430d0000da51ae71a01f07a373c49d85
darken <darken@darken.eu>
2022-10-02 19:03:59 +0200

    When on dashboard force data sync every minute

--- app/src/main/java/eu/darken/octi/main/ui/dashboard/DashboardVM.kt
+++ app/src/main/java/eu/darken/octi/main/ui/dashboard/DashboardVM.kt
@@ -6,24 +6,29 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.SavedStateHandle
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
-import eu.darken.octi.common.BuildConfigWrap
 import eu.darken.octi.common.coroutine.AppScope
 import eu.darken.octi.common.coroutine.DispatcherProvider
 import eu.darken.octi.common.debug.logging.Logging.Priority.WARN
 import eu.darken.octi.common.debug.logging.log
 import eu.darken.octi.common.debug.logging.logTag
 import eu.darken.octi.common.livedata.SingleLiveEvent
+import eu.darken.octi.common.network.NetworkStateProvider
 import eu.darken.octi.common.uix.ViewModel3
 import eu.darken.octi.main.core.GeneralSettings
 import eu.darken.octi.main.ui.dashboard.items.PermissionVH
 import eu.darken.octi.main.ui.dashboard.items.WelcomeVH
 import eu.darken.octi.main.ui.dashboard.items.perdevice.DeviceVH
+import eu.darken.octi.module.core.ModuleData
+import eu.darken.octi.module.core.ModuleManager
 import eu.darken.octi.modules.meta.core.MetaInfo
+import eu.darken.octi.modules.power.core.PowerInfo
 import eu.darken.octi.modules.power.ui.dashboard.DevicePowerVH
 import eu.darken.octi.modules.wifi.core.WifiInfo
 import eu.darken.octi.modules.wifi.ui.dashboard.DeviceWifiVH
+import eu.darken.octi.sync.core.SyncManager
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import java.time.Instant
 import javax.inject.Inject
@@ -36,8 +41,9 @@ class DashboardVM @Inject constructor(
     @ApplicationContext private val context: Context,
     @AppScope private val appScope: CoroutineScope,
     private val generalSettings: GeneralSettings,
-    private val syncManager: eu.darken.octi.sync.core.SyncManager,
-    private val moduleManager: eu.darken.octi.module.core.ModuleManager,
+    private val syncManager: SyncManager,
+    private val moduleManager: ModuleManager,
+    private val networkStateProvider: NetworkStateProvider,
     private val permissionTool: PermissionTool,
 ) : ViewModel3(dispatcherProvider = dispatcherProvider) {
 
@@ -47,19 +53,21 @@ class DashboardVM @Inject constructor(
         val items: List<DashboardAdapter.Item>,
     )
 
-    private val refreshTicker = flow {
+    private val tickerUiRefresh = flow {
         while (currentCoroutineContext().isActive) {
             emit(Instant.now())
-            if (BuildConfigWrap.DEBUG) {
-                delay(1000)
-            } else {
-                delay(60 * 1000)
+            // "Last seen update"
+            val netstate = networkStateProvider.networkState.first()
+            if (netstate.isInternetAvailable && !netstate.isMeteredConnection) {
+                log(TAG) { "Auto data refresh..." }
+                syncManager.sync()
             }
+            delay(60 * 1000)
         }
     }
 
     val listItems: LiveData<State> = combine(
-        refreshTicker,
+        tickerUiRefresh,
         generalSettings.isWelcomeDismissed.flow,
         moduleManager.byDevice,
         permissionTool.missingPermissions
@@ -89,7 +97,7 @@ class DashboardVM @Inject constructor(
         byDevice.devices
             .mapNotNull { (deviceId, moduleDatas) ->
                 val metaModule =
-                    moduleDatas.firstOrNull { it.data is MetaInfo } as? eu.darken.octi.module.core.ModuleData<MetaInfo>
+                    moduleDatas.firstOrNull { it.data is MetaInfo } as? ModuleData<MetaInfo>
                 if (metaModule == null) {
                     log(TAG, WARN) { "Missing meta module for $deviceId" }
                     return@mapNotNull null
@@ -99,11 +107,11 @@ class DashboardVM @Inject constructor(
                     .sortedBy { it.orderPrio }
                     .mapNotNull {
                         when (it.data) {
-                            is eu.darken.octi.modules.power.core.PowerInfo -> DevicePowerVH.Item(
-                                data = it as eu.darken.octi.module.core.ModuleData<eu.darken.octi.modules.power.core.PowerInfo>,
+                            is PowerInfo -> DevicePowerVH.Item(
+                                data = it as ModuleData<PowerInfo>,
                             )
                             is WifiInfo -> DeviceWifiVH.Item(
-                                data = it as eu.darken.octi.module.core.ModuleData<WifiInfo>,
+                                data = it as ModuleData<WifiInfo>,
                             )
                             else -> {
                                 log(TAG, WARN) { "Unsupported module data: ${it.data}" }
@@ -138,12 +146,12 @@ class DashboardVM @Inject constructor(
         if (granted) permissionTool.recheck()
     }
 
-    private val eu.darken.octi.module.core.ModuleData<out Any>.orderPrio: Int
+    private val ModuleData<out Any>.orderPrio: Int
         get() = INFO_ORDER.indexOfFirst { it.isInstance(this.data) }
 
     companion object {
         private val INFO_ORDER = listOf(
-            eu.darken.octi.modules.power.core.PowerInfo::class,
+            PowerInfo::class,
             WifiInfo::class
         )
         private val TAG = logTag("Dashboard", "Fragment", "VM")
