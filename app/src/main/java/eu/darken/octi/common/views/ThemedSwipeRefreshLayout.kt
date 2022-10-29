39d4af9c194ae20f382b415857413cd7006ab63f
darken <darken@darken.eu>
2022-10-29 14:43:29 +0200

    Fix swipe to refresh animation

--- app/src/main/java/eu/darken/octi/common/views/ThemedSwipeRefreshLayout.kt
+++ app/src/main/java/eu/darken/octi/common/views/ThemedSwipeRefreshLayout.kt
@@ -6,11 +6,13 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import eu.darken.octi.R
 import eu.darken.octi.common.getColorForAttr
 
-class ThemedSwipeRefreshLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
-    SwipeRefreshLayout(context, attrs) {
+class ThemedSwipeRefreshLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+) : SwipeRefreshLayout(context, attrs) {
 
     override fun onFinishInflate() {
-        setProgressBackgroundColorSchemeColor(context.getColorForAttr(R.attr.colorPrimarySurface))
+        setProgressBackgroundColorSchemeColor(context.getColorForAttr(R.attr.colorSurface))
         setColorSchemeColors(
             context.getColorForAttr(R.attr.colorPrimary),
             context.getColorForAttr(R.attr.colorPrimaryDark)
