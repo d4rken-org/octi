9b3f833201a7bcc1eb434e73b350ea591198553c
darken <darken@darken.eu>
2022-10-31 14:14:36 +0100

    battery widget wip

---
+++ app/src/main/java/eu/darken/octi/modules/power/ui/widget/BatteryWidgetProvider.kt
@@ -0,0 +1,37 @@
+package eu.darken.octi.modules.power.ui.widget
+
+import android.appwidget.AppWidgetProvider
+
+class BatteryWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // Perform this loop procedure for each widget that belongs to this
+        // provider.
+        appWidgetIds.forEach { appWidgetId ->
+            // Create an Intent to launch ExampleActivity.
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(
+                /* context = */ context,
+                /* requestCode = */  0,
+                /* intent = */ Intent(context, ExampleActivity::class.java),
+                /* flags = */ PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            // Get the layout for the widget and attach an on-click listener
+            // to the button.
+            val views: RemoteViews = RemoteViews(
+                context.packageName,
+                R.layout.appwidget_provider_layout
+            ).apply {
+                setOnClickPendingIntent(R.id.button, pendingIntent)
+            }
+
+            // Tell the AppWidgetManager to perform an update on the current
+            // widget.
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
