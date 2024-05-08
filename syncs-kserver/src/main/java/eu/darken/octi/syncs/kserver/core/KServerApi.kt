5f285a43d5c3e118c1c88f196d46f3cb0d8d1633
darken <darken@darken.eu>
2024-05-08 21:36:32 +0200

    wip

--- syncs-kserver/src/main/java/eu/darken/octi/syncs/kserver/core/KServerApi.kt
+++ syncs-kserver/src/main/java/eu/darken/octi/syncs/kserver/core/KServerApi.kt
@@ -5,7 +5,13 @@ import com.squareup.moshi.JsonClass
 import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Response
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface KServerApi {
 
@@ -15,11 +21,16 @@ interface KServerApi {
         @Json(name = "password") val password: String
     )
 
-    @POST("auth/register")
+    @POST("account")
     suspend fun register(
         @Header("X-Device-ID") deviceID: String,
         @Query("share") shareCode: String? = null,
     ): RegisterResponse
+
+    @DELETE("account")
+    suspend fun delete(
+        @Header("X-Device-ID") deviceID: String,
+    )
 
     @JsonClass(generateAdapter = true)
     data class ShareCodeResponse(
