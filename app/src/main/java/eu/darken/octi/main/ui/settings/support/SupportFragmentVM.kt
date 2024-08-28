f95f258e8ce9156b62985a5292af971733a209bd
darken <darken@darken.eu>
2024-08-28 17:27:03 +0200

    Remove email and add discord link in Settings>Support

--- app/src/main/java/eu/darken/octi/main/ui/settings/support/SupportFragmentVM.kt
+++ app/src/main/java/eu/darken/octi/main/ui/settings/support/SupportFragmentVM.kt
@@ -1,12 +1,7 @@
 package eu.darken.octi.main.ui.settings.support
 
 import android.content.Intent
-import android.os.Build
-import androidx.lifecycle.SavedStateHandle
 import dagger.hilt.android.lifecycle.HiltViewModel
-import eu.darken.octi.common.BuildConfigWrap
-import eu.darken.octi.common.EmailTool
-import eu.darken.octi.common.InstallId
 import eu.darken.octi.common.coroutine.DispatcherProvider
 import eu.darken.octi.common.debug.logging.log
 import eu.darken.octi.common.debug.recording.core.RecorderModule
@@ -17,10 +12,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SupportFragmentVM @Inject constructor(
-    private val handle: SavedStateHandle,
-    private val emailTool: EmailTool,
-    private val installId: InstallId,
-    private val dispatcherProvider: DispatcherProvider,
+    dispatcherProvider: DispatcherProvider,
     private val recorderModule: RecorderModule,
 ) : ViewModel3(dispatcherProvider) {
 
@@ -28,30 +20,6 @@ class SupportFragmentVM @Inject constructor(
     val clipboardEvent = SingleLiveEvent<String>()
 
     val isRecording = recorderModule.state.map { it.isRecording }.asLiveData2()
-
-    fun sendSupportMail() = launch {
-
-        val bodyInfo = StringBuilder("\n\n\n")
-
-        bodyInfo.append("--- Infos for the developer ---\n")
-
-        bodyInfo.append("App version: ").append(BuildConfigWrap.VERSION_DESCRIPTION).append("\n")
-
-        bodyInfo.append("Device: ").append(Build.FINGERPRINT).append("\n")
-        bodyInfo.append("Install ID: ").append(installId.id).append("\n")
-
-        val email = EmailTool.Email(
-            receipients = listOf("support@darken.eu"),
-            subject = "[Octi] Question/Suggestion/Request\n",
-            body = bodyInfo.toString()
-        )
-
-        emailEvent.postValue(emailTool.build(email))
-    }
-
-    fun copyInstallID() = launch {
-        clipboardEvent.postValue(installId.id)
-    }
 
     fun startDebugLog() = launch {
         log { "startDebugLog()" }
