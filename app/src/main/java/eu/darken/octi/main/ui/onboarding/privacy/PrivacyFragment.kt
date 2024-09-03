f02dee3024fec9a1a6161d9453e8003aa5bea9ed
darken <darken@darken.eu>
2024-09-03 08:42:42 +0200

    Nicer onboarding

--- app/src/main/java/eu/darken/octi/main/ui/onboarding/privacy/PrivacyFragment.kt
+++ app/src/main/java/eu/darken/octi/main/ui/onboarding/privacy/PrivacyFragment.kt
@@ -1,4 +1,4 @@
-package eu.darken.octi.main.ui.onboarding.ui
+package eu.darken.octi.main.ui.onboarding.privacy
 
 import android.os.Bundle
 import android.view.View
@@ -9,21 +9,21 @@ import eu.darken.octi.common.PrivacyPolicy
 import eu.darken.octi.common.WebpageTool
 import eu.darken.octi.common.uix.Fragment3
 import eu.darken.octi.common.viewbinding.viewBinding
-import eu.darken.octi.databinding.OnboardingFragmentBinding
+import eu.darken.octi.databinding.OnboardingPrivacyFragmentBinding
 import javax.inject.Inject
 
 
 @AndroidEntryPoint
-class OnboardingFragment : Fragment3(R.layout.onboarding_fragment) {
+class PrivacyFragment : Fragment3(R.layout.onboarding_privacy_fragment) {
 
-    override val vm: OnboardingFragmentVM by viewModels()
-    override val ui: OnboardingFragmentBinding by viewBinding()
+    override val vm: PrivacyFragmentVM by viewModels()
+    override val ui: OnboardingPrivacyFragmentBinding by viewBinding()
 
     @Inject lateinit var webpageTool: WebpageTool
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         ui.goPrivacyPolicy.setOnClickListener { webpageTool.open(PrivacyPolicy.URL) }
-        ui.continueAction.setOnClickListener { vm.finishOnboarding() }
+        ui.continueAction.setOnClickListener { vm.finishScreen() }
         super.onViewCreated(view, savedInstanceState)
     }
 }
