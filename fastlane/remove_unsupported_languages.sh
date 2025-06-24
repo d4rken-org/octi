d5e270d81c43d1ba8217b7b6a9b0dcf156eff837
darken <darken@darken.eu>
2025-06-24 19:27:29 +0200

    Remove `ckb-IR` and `pa-IN` before GPlay submission (unsupported)

--- fastlane/remove_unsupported_languages.sh
+++ fastlane/remove_unsupported_languages.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Removing unsupported languages..."
 echo `pwd`
+rm -rv ./metadata/android/ckb-IR
 rm -rv ./metadata/android/es-AR
 rm -rv ./metadata/android/sc-IT
 rm -rv ./metadata/android/sq-AL
@@ -12,5 +13,6 @@ rm -rv ./metadata/android/kmr-TR
 rm -rv ./metadata/android/ur-IN
 rm -rv ./metadata/android/zu
 rm -rv ./metadata/android/si-LK
+rm -rv ./metadata/android/pa-IN
 find ./metadata/android -empty -type d -delete
 exit 0
